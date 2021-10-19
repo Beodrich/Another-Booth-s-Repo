c86cba85fd5d7fa6b148cf51e4b28dd25d43f467
Beodrich <60353906+Beodrich@users.noreply.github.com>
2021-10-19 13:30:21 -0500

    more basic code

--- src/src/BoothAlgorithm.java
+++ src/src/BoothAlgorithm.java
@@ -1,9 +1,21 @@
 import java.util.Scanner;
 
 public class BoothAlgorithm {
+    public static String ConvertIntToBinary(int number){
+        if(number>=0){
+
+        }
+        else{
+
+        }
+        return null;
+    }
+
      public static void main(String[] args) {
        int numberOne=0;
        int numberTwo=0;
+       String numOneAsString;
+       String numTwoAsString;
 
         System.out.println("hello cole");
 
@@ -13,6 +25,9 @@ public class BoothAlgorithm {
         System.out.println("Enter your second number");
         numberTwo=src.nextInt();
         System.out.println("first number: "+ numberOne + "second number "+ numberTwo);
+        numOneAsString=ConvertIntToBinary(numberOne);
+        numTwoAsString=ConvertIntToBinary(numberTwo);
+
 
     }
 }
