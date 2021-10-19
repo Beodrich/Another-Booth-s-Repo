fb46c2e9b902b8703e19f29fb97c77f1821ad9b1
Beodrich <60353906+Beodrich@users.noreply.github.com>
2021-10-19 13:17:59 -0500

    added some basic print statments

--- src/src/BoothAlgorithm.java
+++ src/src/BoothAlgorithm.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class BoothAlgorithm {
      public static void main(String[] args) {
+       int numberOne=0;
+       int numberTwo=0;
+
         System.out.println("hello cole");
+
+        Scanner src= new Scanner(System.in);
+        System.out.println("Enter your first number");
+        numberOne=src.nextInt();
+        System.out.println("Enter your second number");
+        numberTwo=src.nextInt();
+        System.out.println("first number: "+ numberOne + "second number "+ numberTwo);
+
     }
 }
