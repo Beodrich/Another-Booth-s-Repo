af4aea0ed04ebd9810c382f906d78567dc2dd9d4
Cole <49996960+CWoulf@users.noreply.github.com>
2021-10-21 11:36:17 -0500

    Testing

--- src/src/BoothAlgorithm.java
+++ src/src/BoothAlgorithm.java
@@ -4,17 +4,18 @@ public class BoothAlgorithm {
     public static String ConvertIntToBinary(int number){
 
         // Built-In Java Function that converts int to a binary string
+        //  TODO: This does not fully work normally with negative numbers yet
         String result = Integer.toBinaryString(number);
 
-        String resultWithPadding = String.format("%4s", result).
-                replaceAll(" ", "0");  // 4-bit Integer
+        String resultWithPadding = String.format("%8s", result).
+                replaceAll(" ", "0");  // 8-bit Integer
 
         return  resultWithPadding;
     }
 
     public static void DisplayTable(String MCand)
     {
-        System.out.println("Iteration | Step        | Multiplicand  | Product");
+        System.out.println("Iteration | Step        | Multiplicand      | Product");
         for(int i = 0; i <=8; i++)
         {
             System.out.println("_______________________________________________________");
@@ -32,20 +33,15 @@ public class BoothAlgorithm {
         System.out.println("Hello Ben!");
 
         Scanner src= new Scanner(System.in);
-        System.out.println("Please Enter your first number");
+        System.out.println("Please Enter the Multiplicand");
         numberOne=src.nextInt();
-        System.out.println("Please Enter your second number");
+        System.out.println("Please Enter the Multiplier");
         numberTwo=src.nextInt();
         numOneAsString=ConvertIntToBinary(numberOne);
         numTwoAsString=ConvertIntToBinary(numberTwo);
 
-
-        //numOneAsString = Integer.toBinaryString(numberOne);
-        //numTwoAsString = Integer.toBinaryString(numberTwo);
-
          System.out.println("first number "+ numberOne+" as a binary number is: "+ numOneAsString +
                  " and the second number "+ numberTwo+" a binary number is: "+ numTwoAsString);
-
 
         DisplayTable(numOneAsString);
 
