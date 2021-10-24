e44e230bf15ace04fef378e94f3111235800285c
Beodrich <60353906+Beodrich@users.noreply.github.com>
2021-10-24 16:33:27 -0500

    finished the booths

--- src/src/BoothAlgorithm.java
+++ src/src/BoothAlgorithm.java
@@ -89,24 +89,24 @@ public class BoothAlgorithm {
             firstNumber[i] = firstNumber[i - 1];
         }
         firstNumber[0] = signNumber;
-        for (int i : firstNumber) {
+        /*for (int i : firstNumber) {
             System.out.print(i + " ");
         }
-        System.out.print(" ");
+        System.out.print(" ");*/
 
         for (int i = secondNumber.length - 1; i >= 1; i--) {
             secondNumber[i] = secondNumber[i - 1];
         }
         secondNumber[0] = carryOverNumber;
-        for (int i : secondNumber) {
+        /*for (int i : secondNumber) {
             System.out.print(i + " ");
         }
-        System.out.println(" ");
+        System.out.println(" ");*/
     }
     public static int[] Add(int [] firstBinaryNumber, int[] secondBinaryNumbers){
         int addedNumber[]= new int[8];
         int addedNumberIndex=0;
-         boolean carry=false;
+        boolean carry=false;
         for(int i= firstBinaryNumber.length-1; i>=0; --i){
             int firstNum=firstBinaryNumber[i];
             int secondNum= secondBinaryNumbers[i];
@@ -134,11 +134,11 @@ public class BoothAlgorithm {
                 //1 1
                 carry=true;
                 addedNumber[addedNumberIndex++]=0;
-                }
             }
+        }
         Reverse(addedNumber);
         return addedNumber;
-        }
+    }
 
     public static int[] ConvertIntToBinary(int number){
         int numberAsBinary[]= new int[8];
@@ -163,67 +163,71 @@ public class BoothAlgorithm {
         }
         return num;
     }
-    public static void DisplayTable(String currentIteration, String step, int[]mcand, int[]productFirst, int[]productSecond) {
+    public static void DisplayTable(int currentIteration, String step,int[]mcand,
+                                    int[]A_productFirst, int[]A_productSecond,
+                                    int[]B_productFirst, int[]B_productSecond) {
 
 
-            System.out.println("_______________________________________________________");
-            System.out.println(currentIteration + "         | " + step + "         | "+ PrintArray(mcand)+ "\t|" + PrintArray(productFirst)+ PrintArray(productSecond));
-
+        System.out.println("_______________________________________________________________________");
+        System.out.println(currentIteration + "         |     " + step + "    | "+ PrintArray(mcand)+ "\t   | "
+                + PrintArray(B_productFirst)+ " " +PrintArray(B_productSecond));
+        if(currentIteration != 0){
+            System.out.println("          |     ASR 1              | "+ PrintArray(mcand)+ "\t   | "
+                    + PrintArray(A_productFirst)+ " " +PrintArray(A_productSecond));
+        }
     }
 
     public static void DoTheBooth(int[]mCand, int[]multi){
-        System.out.println("Iteration | Step        | Multiplicand      | Product");
+        System.out.println("Iteration |      Step              | Multiplicand  | Product");
         int[] secondHalf= new int[9];
         int[] firstHalf= new int[8];
+
+        int[] B_secondHalf= new int[9];
+        int[] B_firstHalf= new int[8];
         String step="";
         for(int i=0; i<8; ++i){
             secondHalf[i]=multi[i];
         }
         secondHalf[8]=0;
-        DisplayTable(0, "Init", mCand,firstHalf,secondHalf);
+        DisplayTable(0, "Init           ",mCand,firstHalf,secondHalf,firstHalf,secondHalf);
         for(int i=0; i<8;++i){
             //0 1 and 1 0
             int firstNum= secondHalf[secondHalf.length-2];
             int secondNum= secondHalf[secondHalf.length-1];
             if(firstNum==1 && secondNum==0){
                 firstHalf=Add(firstHalf,NumberInTwoComplement(mCand));
-                step="Prod= Prod-Mcand\nShiftRight";
-                DisplayTable(String.valueOf(i+1), step, mCand, firstHalf,secondHalf);
+                step="Prod=Prod-Mcand";
             }
             else if(firstNum==0 && secondNum==1){
                 firstHalf=Add(firstHalf,mCand);
-                step="Prod= Prod+Mcand\nShiftRight";
-
+                step="Prod=Prod+Mcand";
             }
             else{
-                step="No Op\nShiftRight";
-
+                step="No Operation   ";
             }
+            B_firstHalf = firstHalf.clone();
+            B_secondHalf = secondHalf.clone();
             ShiftRight(firstHalf,secondHalf);
-           // DisplayTable(i+1,step,mCand,firstHalf,secondHalf);
+            DisplayTable(i+1,step,mCand,firstHalf,secondHalf, B_firstHalf, B_secondHalf);
+
 
         }
-        System.out.println("final number is ");
+        System.out.println("_______________________________________________________________________");
+        System.out.println("Our Final Product is: ");
         for(int i: firstHalf){
             System.out.print(i + " ");
         }
         for(int i=0; i<8;++i){
             System.out.print(secondHalf[i] + " ");
         }
-
-
-
-
-
-
     }
 
-     public static void main(String[] args) {
-       int numberOne=0;
-       int numberTwo=0;
+    public static void main(String[] args) {
+        int numberOne=0;
+        int numberTwo=0;
 
-       int numberOneAsBinary[]= new int[8];
-       int numberTwoasBinary[]= new int[8];
+        int numberOneAsBinary[]= new int[8];
+        int numberTwoasBinary[]= new int[8];
 
         System.out.println("hello cole");
         System.out.println("Hello Ben!");
@@ -236,16 +240,16 @@ public class BoothAlgorithm {
         numberOne=src.nextInt();
         System.out.println("Please Enter the Multiplier");
         numberTwo=src.nextInt();
-         //numberOneAsBinary=ConvertIntToBinary(numberOne);
+        //numberOneAsBinary=ConvertIntToBinary(numberOne);
         //numberTwoasBinary=ConvertIntToBinary(numberTwo);
 
-         //System.out.println("first number "+ numberOne+" as a binary number is: "+ numOneAsString +
-               //  " and the second number "+ numberTwo+" a binary number is: "+ numTwoAsString);*/
+        //System.out.println("first number "+ numberOne+" as a binary number is: "+ numOneAsString +
+        //  " and the second number "+ numberTwo+" a binary number is: "+ numTwoAsString);*/
 
-         // DisplayTable(numOneAsString);
-         //testing to make sure numbers work
-         numberOneAsBinary=DetermineConversionFromUserNumber(numberOne);
-         numberTwoasBinary= DetermineConversionFromUserNumber(numberTwo);
+        // DisplayTable(numOneAsString);
+        //testing to make sure numbers work
+        numberOneAsBinary=DetermineConversionFromUserNumber(numberOne);
+        numberTwoasBinary= DetermineConversionFromUserNumber(numberTwo);
          /*System.out.println("Number One "+ numberOne + " as binary");
          for(int i: numberOneAsBinary){
              System.out.print(i + " ");
@@ -259,6 +263,6 @@ public class BoothAlgorithm {
          System.out.println();
          System.out.println("Shifted Right is:");
          ShiftRight(numberOneAsBinary,numberTwoasBinary);*/
-         DoTheBooth(numberOneAsBinary,numberTwoasBinary);
-     }
+        DoTheBooth(numberOneAsBinary,numberTwoasBinary);
+    }
 }
